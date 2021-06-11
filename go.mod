81d3fa3ed3c6fa4d58307d540e8a76ce7747f4f3
nkmr-jp <nkmr.git@gmail.com>
2021-06-11 12:36:00 +0900

    Update go files

--- go.mod
+++ go.mod
@@ -2,10 +2,9 @@ module irita-sample
 
 go 1.15
 
-require (
-	github.com/bianjieai/bsnhub-sdk-go v1.0.0 // indirect
-	github.com/bianjieai/irita-sdk-go v1.1.0
-)
+//github.com/bianjieai/bsnhub-sdk-go v1.0.0 // indirect
+//github.com/bianjieai/irita-sdk-go v0.0.0-20210426091508-29597b871513
+require github.com/bianjieai/irita-sdk-go v1.1.0
 
 replace github.com/tendermint/tendermint => github.com/bianjieai/tendermint v0.34.8-irita-210413
 
--- main.go
+++ main.go
@@ -1,24 +1,51 @@
 package main
 
 import (
-	sdk "github.com/bianjieai/bsnhub-sdk-go"
+	"os"
+
+	sdk "github.com/bianjieai/irita-sdk-go"
+	// sdk "github.com/bianjieai/irita-sdk-go"
 	"github.com/bianjieai/irita-sdk-go/types"
+	"github.com/bianjieai/irita-sdk-go/types/store"
+	// sdk "github.com/bianjieai/bsnhub-sdk-go"
 )
 
+// SDK の初期化
+// http://localhost:8080/SDK/Go_SDK/overview.html
+// https://github.com/irisnet/irishub-sdk-go/issues/69
+// https://github.com/irisnet/irishub-sdk-go
+// http://localhost:8080/quick_start/sdk_init.html
 func main() {
+	// 定义客户端配置变量
+	nodeURI := "tcp://localhost:26657"
+	// network := types.Mainnet
+	chainID := "test"
+	gas := 200000
+	fee := types.NewDecCoins(types.NewDecCoin("point", types.NewInt(5)))
+	mode := types.Commit
+	algo := "sm2"
+	timeout := 10
+	level := "info"
+	dbPath, _ := os.Getwd()
 
-	// SDK の初期化
-	// http://localhost:8080/SDK/Go_SDK/overview.html
-	options := []types.Option{
-		types.KeyDAOOption(store.NewMemory(nil)),
-		types.TimeoutOption(10),
+	// 生成客户端配置对象
+	clientConfig := types.ClientConfig{
+		NodeURI: nodeURI,
+		// Network:   "network",
+		ChainID: chainID,
+		Gas:     uint64(gas),
+		Fee:     fee,
+		Mode:    mode,
+		Algo:    algo,
+		KeyDAO:  store.NewFileDAO(dbPath),
+		Timeout: uint(timeout),
+		Level:   level,
 	}
 
-	cfg, err := types.NewClientConfig(nodeURI, chainID, options...)
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	client := sdk.NewIRITAClient(cfg)
+	client := sdk.NewIRITAClient(clientConfig)
 	println(client)
 }
